d6bc4c851d761bce3c9aa6edc4005721254d7e70
PyGodoy <godoyvitorio99@gmail.com>
2025-02-05 11:52:42 -0300

    download app

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.r5m2cfs0qq"
+    "revision": "0.ga79n5lbet8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {

fd41ccf6d319d1854012150ac7ff30654c21e209
PyGodoy <114434952+PyGodoy@users.noreply.github.com>
2025-02-14 12:41:15 -0300

    let

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -39,7 +39,7 @@ export default function Login() {
         throw new Error('Por favor, insira seu email');
       }
 
-      let { data, error } = await supabase.auth.resetPasswordForEmail(email)
+      let { data, error } = await supabase.auth.resetPasswordForEmail(email);
 
       if (error) {
         console.error('Erro ao resetar senha:', error);
